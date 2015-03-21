08c4c9101a1d547d3fa9d434f945a880ae409eed
Denzel Pressey <pressey90@gmail.com>
2015-03-21 15:51:15 -0400

    a + b problem

---
+++ IntegerAddition.java
@@ -0,0 +1,13 @@
+package timus_online_judge;
+
+public class IntegerAddition {
+	
+	
+	public static int addition(int a, int b){
+		return a + b;
+	}
+	
+	public static void main(String[] args){
+		System.out.println( IntegerAddition.addition(2, 2) );
+	}
+}
